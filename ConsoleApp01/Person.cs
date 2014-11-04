5a23d821d974125a36ba99e57581bfa4bc31fa1f
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-11-04 12:47:45 -0800

    adding files

---
+++ ConsoleApp01/Person.cs
@@ -0,0 +1,15 @@
+namespace TempSamples.ConsoleApp01
+{
+    public class Person
+    {
+		public string FirstName{
+			get;
+			set;
+		}
+
+		public string LastName{
+			get;
+			set
+		}
+    }
+}
